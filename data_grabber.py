ba3ca93c580fdbebf34f1a5b3c8dedec63684f08
pmoreira1 <pedrodavidmoreira@gmail.com>
2020-03-03 12:53:51 +0100

    Cleaned up the comments a little bit

--- data_grabber.py
+++ data_grabber.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
+
+# URLS OF CSV FILES TO IMPORT
 containers_url = 'https://api.data.amsterdam.nl/afval/v1/containers/?detailed=1&format=csv&page_size=15000'
 wells_url = 'https://api.data.amsterdam.nl/afval/v1/wells/?format=csv&page_size=15000'
 container_types_url = 'https://api.data.amsterdam.nl/afval/v1/containertypes/?format=csv&page_size=15000'
+
+# DATABASE DETAILS TO SAVE INFORMATION AT END OF SCRIPT.
 remote_db = create_engine('mysql+pymysql://student:IHisCool!@34.77.233.175/waste')
 
 print("======STARTING IMPORT======")
-# Grab containers and create a smaller more relevant list. Fields can be added later if needed
+# Working on Containers
 print("Grabbing latest containers data...")
 container_list = pd.read_csv(containers_url)
-# print(container_list.columns)
 print("Cleaning containers data and saving locally")
 containers = container_list[['id', 'active', 'container_type.id', 'waste_name', 'waste_type', 'well.id', 'placing_date']]
 containers = containers.rename(columns={
@@ -20,7 +23,7 @@ containers = containers.rename(columns={
 }).set_index('container_id')
 container_list.to_csv('data/containers.csv')
 
-# Import wells and store it in a smaller list.
+# Working on Wells
 print("Grabbing latest wells data")
 well_list = pd.read_csv(wells_url)
 print("Cleaning wells and saving locally")
@@ -28,7 +31,7 @@ wells = well_list[['id', 'stadsdeel', 'buurt_code', 'geometrie.type', 'address.n
 wells = wells.rename(columns={'id': 'well_id', 'geometrie.type': 'well_type', 'address.summary': 'well_address', 'address.neighbourhood': 'well_neighbourhood', 'geometrie.coordinates.0': 'well_longitude', 'geometrie.coordinates.1': 'well_latitude'}).set_index('well_id')
 wells.to_csv('data/wells.csv')
 
-# Import container types and store it in a smaller list.
+# Working on Container Types
 print("Grabbing latest container types")
 container_types = pd.read_csv(container_types_url)
 print("Cleaning container types and saving locally")
@@ -37,7 +40,8 @@ container_types = container_types.rename(columns={
     'id': 'container_type_id'
 }).set_index('container_type_id')
 container_types.to_csv('data/container_types.csv')
-# Import population count
+
+# Working on Population Data
 print("Grabbing latest population data")
 popuplation_data = pd.read_excel('data/2020-stadsdelen.xlsx').reset_index()
 print("Cleaning population data and saving locally")
@@ -46,12 +50,11 @@ popuplation_data = popuplation_data.rename(columns={
     'wijk/std': 'buurt_code',
     'naam wijk/std': 'buurt'
 }).dropna().set_index('buurt_id')
-# store to db
+
+# Saving information in database
 print("Storing all data in database....")
 containers.to_sql('containers', remote_db, if_exists='replace', index='container_id')
 container_types.to_sql('container_types', remote_db, if_exists='replace', index='container_type_id')
 wells.to_sql('wells', remote_db, if_exists='replace', index='well_id')
 popuplation_data.to_sql('popuplation_data', remote_db, if_exists='replace', index='buurt_id')
 print("======IMPORT COMPLETE======")
-
-
